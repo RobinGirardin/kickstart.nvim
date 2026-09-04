1b5086852e1f540101c8d575945bae4c4fe6a3cc
Robin Girardin <116655454+RobinGirardin@users.noreply.github.com>
2026-09-04 11:27:11 +0200

    udpate: send to neovim terminal

--- lua/custom/plugins/vim-slime.lua
+++ lua/custom/plugins/vim-slime.lua
@@ -3,8 +3,17 @@
 return {
   'jpalardy/vim-slime',
   config = function()
-    vim.g.slime_target = 'tmux'
-    vim.g.slime_default_config = { socket_name = 'default', target_pane = '{last}' }
+    vim.g.slime_target = 'neovim'
+    vim.g.slime_suggest_default = 1
+    vim.g.slime_menu_config = 1
+    vim.g.slime_neovim_ignore_unlisted = 0
+    vim.g.slime_neovim_menu_order = {
+      { name = 'buffer name: ' },
+      { pid = 'shell process identifier: ' },
+      { jobid = 'neovim internal job identifier: ' },
+      { term_title = 'process or pwd: ' }
+    }
+    vim.g.slime_neovim_menu_delimiter = ' | '
     vim.g.slime_python_ipython = 1
     vim.g.slime_bracketed_paste = 1
     vim.keymap.set('v', '<leader>ss', '<Plug>SlimeRegionSend')
